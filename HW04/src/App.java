21ad5cd9b0632a3e296a50bcad46f12482a9aa35
Nikolay Tikhomirov <79159489230@yandex.ru>
2023-04-05 21:59:19 +0300

    fourth HW complete

---
+++ HW04/src/App.java
@@ -0,0 +1,46 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hello, World!");
+        // Приложение, которое:
+        // Принимает от пользователя и запоминает строки
+        // Если пользователь ввел print, то выводим все предыдущие строки в обратном порядке
+        // Если введено revert, то удаляем последнюю введеную строку
+        // Если введено exit, то завершаем программу
+
+        // java
+        // c++
+        // python
+        // c#
+        // print
+        // < [c#, python, c++, java]
+        // revert
+        // print
+        // < [python, c++, java]
+        // exit
+
+        Scanner sc = new Scanner(System.in);
+        List<String> storage = new LinkedList<>();
+        String input;
+        while (!(input = sc.nextLine()).equals("exit")) {
+            String userInter = input;
+            if ("revert".equals(userInter)) Collections.reverse(storage);
+            else if ("print".equals(userInter)) {
+                // Вывод строкой, но, вероятно, это будет ресурсозатратно, при больших объемах
+                // List<String> newStorage = new LinkedList<>(storage);
+                // Collections.reverse(newStorage);
+                // System.out.println(newStorage);
+
+                // Вывод поэлементно, но работать будет быстрее
+                ListIterator<String> listIterator = storage.listIterator(storage.size());
+                while (listIterator.hasPrevious()) System.out.println(listIterator.previous());
+            }
+            else storage.add(input);
+        }
+    }
+}
